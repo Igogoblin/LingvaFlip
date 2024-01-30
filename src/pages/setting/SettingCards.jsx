3c7f8138909c1ec519c4cd24d2c3a9d362b72310
Ihar Skavysh <igogoblin@gmail.com>
2024-01-30 16:14:28 +0300

    feat: add checkbox in settings

--- src/pages/setting/SettingCards.jsx
+++ src/pages/setting/SettingCards.jsx
@@ -1,5 +1,21 @@
+import s from "./settingsCard.module.css";
+
 function SettingCards() {
-  return <div>This is setting for cards</div>;
+  return (
+    <section className={s.section_setting}>
+      <div className={s.wrapper_setting}>
+        <h3>Настройка карточек</h3>
+        <p className={s.settings_text}>
+          Вы можете выбрать конкретную тему для изучения или не останавливаться
+          на чем-то одном и изучать слова из разных тем одновременно:
+        </p>
+        <form className={s.check_area}>
+          <input type="checkbox" className={s.checkRandom}></input>{" "}
+          <span>Все темы рандомно</span>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default SettingCards;
