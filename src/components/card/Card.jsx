6bc0cad034e71be88832016ab18590de33fa7909
Ihar Skavysh <igogoblin@gmail.com>
2024-02-01 14:03:26 +0300

    feat: add base lyout card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,15 +9,29 @@ function Card() {
   // };
 
   return (
-    <div
-      // className={`card_${isFlipped ? "flipped" : ""}`}
-      // onClick={handleCardClick}
-      className={s.card}
-    >
-      this is Card
-      <div className={s.front}>Front</div>
-      <div className={s.back}>Back</div>
-    </div>
+    <section>
+      <div className={s.card_area}>
+        <div className={s.card_box}>
+          <h4>Thema</h4>
+          <div
+            // className={`card_${isFlipped ? "flipped" : ""}`}
+            // onClick={handleCardClick}
+            className={s.card}
+          >
+            this is Card
+            <div className={s.front}>Front</div>
+            <div className={s.back}>Back</div>
+          </div>
+          <div className={s.card_panel}>
+            <button className={s.button_skip}>Пропустить</button>
+            <div className={s.card_buttons}>
+              <button className={s.button_know}>Не знаю</button>
+              <button className={s.button_dont}>Знаю</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
