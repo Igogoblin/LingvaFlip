c5983679294ee9b9c06a9728368db0ab280e48f8
Ihar Skavysh <igogoblin@gmail.com>
2024-02-19 21:46:04 +0300

    feat: try add Map for studied

--- src/pages/studied/Studied.jsx
+++ src/pages/studied/Studied.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 const Studied = () => {
   const studied = useSelector((state) => state.cards.words);
+  const [thema /*setThema*/] = useState(studied[0].subject);
+
   console.log(studied);
+  console.log(thema);
+  // const wordsThema = () => {
+  //   studied.map((el) => {
+  //     if (el.subject === thema && el.study === false) {
+  //       console.log("zashel !!!");
+  //       return el;
+  //     }
+  //   });
+  // };
+  // console.log(wordsThema());
   return (
     <div>
       <h3>this is Studied page</h3>
@@ -13,6 +26,34 @@ const Studied = () => {
         })} */}
         {/* <li>map</li> */}
       </ul>
+      {/* {studied.map((words)=>{
+        <ul>
+          
+        </ul>
+      })} */}
+      {/* {studied.forEach((element) => {
+        console.log(element);
+        if (element.subject == thema) {
+          <ul></ul>
+        }
+      })} */}
+      {/* {studied.map((element) => {
+        if (element.subject === thema) {
+          ("");
+        } else {
+          <ul>
+            {thema}
+            {studied.map((el) => {
+              if (el.subject === thema && el.studied === false) {
+                <li>{el.word}</li>;
+              }
+            })}
+          </ul>;
+          {
+            setThema(element.subject);
+          }
+        }
+      })} */}
     </div>
   );
 };
--- src/store/cardSlice.js
+++ src/store/cardSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import allWords from "./words.json";
-
+import { enableMapSet } from "immer";
+enableMapSet();
 const cardSlice = createSlice({
   name: "cards",
   initialState: {
@@ -9,6 +10,8 @@ const cardSlice = createSlice({
     now: 1,
     card: undefined,
     random: false,
+    studied: new Map(),
+    // studied: [],
   },
   reducers: {
     showJ(state) {
@@ -27,12 +30,15 @@ const cardSlice = createSlice({
       const studAll = state.words.find((word) => word.id === action.payload);
       stud.study = false;
       studAll.study = false;
+      state.studied.delete(stud.subject, stud);
     },
     study(state, action) {
       const stud = state.activeWords.find((word) => word.id === action.payload);
       const studAll = state.words.find((word) => word.id === action.payload);
       stud.study = true;
       studAll.study = true;
+      console.log(stud);
+      state.studied.set(stud.subject, stud);
     },
     takeCard(state) {
       state.card = state.activeWords[state.now];
@@ -40,6 +46,8 @@ const cardSlice = createSlice({
     changeNow(state) {
       state.now = state.now + 1;
     },
+    // studiedCards(state,action){}
+
     // showStudy(state){
 
     // }
