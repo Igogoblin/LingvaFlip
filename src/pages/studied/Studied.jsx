943cb57d1b3c3309ad369d352833c53ebce6394e
Ihar Skavysh <igogoblin@gmail.com>
2024-02-22 21:53:48 +0300

    feat: add base layout popap

--- src/pages/studied/Studied.jsx
+++ src/pages/studied/Studied.jsx
@@ -3,11 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import ThemaStudied from "./ThemaStudied";
 import s from "./studied.module.css";
 import { resetAllCards } from "../../store/cardSlice";
+import { useState } from "react";
 // import { useState } from "react";
 
 const Studied = () => {
   const studied = useSelector((state) => state.cards.words);
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
 
   console.log(studied);
 
@@ -18,15 +23,43 @@ const Studied = () => {
   });
   console.log(count);
 
+  function resetAll() {
+    dispatch(resetAllCards());
+    togglePopup();
+  }
+
   return (
     <div>
       <div className={s.all_themas}>
         <h3>Список изученных слов</h3>
-        <button onClick={() => dispatch(resetAllCards())}>
+        <button
+          onClick={
+            // () => dispatch(resetAllCards())
+            togglePopup
+          }
+        >
           Сбросить все карточки
         </button>
       </div>
+      {isOpen && (
+        <div className={s.popup_overlay} onClick={togglePopup}>
+          <div className={s.popup_content} onClick={(e) => e.stopPropagation()}>
+            <img src="./assets/ExclamationCircle.svg" alt="attention" />
+            <div>
+              <h5>Сброс изученного матиала</h5>
 
+              <p>
+                Если вы сбросите уже изученный материал, слова будут добавлены в
+                список изучаемых слов
+              </p>
+              <div className={s.resetButton}>
+                <button onClick={togglePopup}>Отмена</button>
+                <button onClick={resetAll}>Сбросить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <>
         {count.map((el, index) => (
           <ThemaStudied thema={el} key={index} option={count} />
