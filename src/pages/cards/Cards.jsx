8ab6de242aefb4a99ff8118bf71d03c03a553ff3
Ihar Skavysh <igogoblin@gmail.com>
2024-03-27 22:51:39 +0300

    refactor: dell comment in cards file

--- src/pages/cards/Cards.jsx
+++ src/pages/cards/Cards.jsx
@@ -1,20 +1,10 @@
 import Card from "../../components/card/Card";
 import s from "./cards.module.css";
-// import { useSelector } from "react-redux";
-// import { useState } from "react";
 
 function Cards() {
-  // const cards = useSelector((state) => state.cards.words);
-  // console.log(cards);
-  // const [count, setCount] = useState(0);
   return (
     <section className={s.cards_area}>
-      {/* {cards.map((card) => (
-        <Card key={card.id} {...card}></Card>
-      ))} */}
-      {/* {bed idea, because we see all cards} */}
       <Card></Card>
-      {/* this is cards area */}
     </section>
   );
 }
